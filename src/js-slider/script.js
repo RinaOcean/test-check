57d63a561d3dac9357c28b79ddc7be7204539e0a
YanaGuzienko <69dusha69@ukr.net>
2020-11-17 14:05:51 +0200

    добавляет слайдер

--- src/js-slider/script.js
+++ src/js-slider/script.js
@@ -1,22 +1,36 @@
 $(document).ready(function () {
   $('.slider-container').slick({
     slidesToShow: 1,
-    slidesToScroll: 1,
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
-    arrows: false,
+    // arrows: false,
     fade: true,
     asNavFor: '.slider-list',
   });
   $('.slider-list').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToShow: 7,
+    slidesToScroll: 3,
     asNavFor: '.slider-container',
     // dots: true,
     centerMode: true,
     focusOnSelect: true,
-    // infinite: true,
-    // speed: 500,
-    // fade: true,
   });
+  //   $('.slider-list').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //        slidesToShow: 2,
+  //        slidesToScroll: 2
+  //       },{
+  //       breakpoint: 480,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '40px',
+  //         slidesToShow: 1
+  //       }
+  //     }
+  //     }]
+  // });
 });
