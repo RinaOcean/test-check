0689f222321a9554cec8fda5a36e260866d2af98
YanaGuzienko <69dusha69@ukr.net>
2020-11-17 14:51:38 +0200

    ставить по три фото на мобилке

--- src/js-slider/script.js
+++ src/js-slider/script.js
@@ -1,36 +1,63 @@
 $(document).ready(function () {
   $('.slider-container').slick({
     slidesToShow: 1,
+    slidesToScroll: 1,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
     // arrows: false,
     fade: true,
     asNavFor: '.slider-list',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
-  $('.slider-list').slick({
-    slidesToShow: 7,
-    slidesToScroll: 3,
-    asNavFor: '.slider-container',
-    // dots: true,
-    centerMode: true,
-    focusOnSelect: true,
-  });
-  //   $('.slider-list').slick({
+  // $('.slider-icon-photo').slick({
   //   slidesToShow: 3,
-  //   slidesToScroll: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //        slidesToShow: 2,
-  //        slidesToScroll: 2
-  //       },{
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 1
-  //       }
-  //     }
-  //     }]
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-container',
+  //   // dots: true,
+  //   centerMode: true,
+  //   focusOnSelect: true,
+
+  //   infinite: true,
+  //   // speed: 500,
+  //   // fade: true,
   // });
+  $('.slider-list').slick({
+    infinite: false,
+    speed: 300,
+    focusOnSelect: true,
+    // slidesToShow: 4,
+    // slidesToScroll: 4,
+    // centerMode: true,
+    asNavFor: '.slider-container',
+    responsive: [
+      {
+        breakpoint: 1366,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 });
