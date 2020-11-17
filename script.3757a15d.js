4d25f69ffec6a4110857a95707d4afe78a0d832c
RinaOcean <antyfifa@gmail.com>
2020-11-17 13:30:30 +0200

    Updates

--- script.3757a15d.js
+++ script.3757a15d.js
@@ -119,7 +119,57 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"js-slider/script.js":[function(require,module,exports) {
 $(document).ready(function () {
-  $('.slider-container').slick({});
+  $('.slider-container').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    // arrows: false,
+    fade: true,
+    asNavFor: '.slider-list'
+  });
+  $('.slider-list').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    asNavFor: '.slider-container',
+    // infinite: true,
+    centerMode: true,
+    focusOnSelect: true
+  }); // $('.slider-list').slick({
+  //   dots: true,
+  //   infinite: false,
+  //   speed: 300,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 4,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1366,
+  //       settings: {
+  //         slidesToShow: 7,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         // dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 7,
+  //         slidesToScroll: 3,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //       },
+  //     },
+  //     // You can unslick at a given breakpoint now by adding:
+  //     // settings: "unslick"
+  //     // instead of a settings object
+  //   ],
+  // });
 });
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -149,7 +199,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53889" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55856" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
