2bdcbeb4c461c67c4d2c3a789382ada72eca9799
YanaGuzienko <69dusha69@ukr.net>
2020-11-18 02:45:47 +0200

    ставит фокус

--- src/js-slider/script.js
+++ src/js-slider/script.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
   $('.slider-container').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
-    // arrows: false,
     fade: true,
     asNavFor: '.slider-list',
     responsive: [
@@ -14,46 +11,22 @@ $(document).ready(function () {
           arrows: false,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
-  // $('.slider-icon-photo').slick({
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   asNavFor: '.slider-container',
-  //   // dots: true,
-  //   centerMode: true,
-  //   focusOnSelect: true,
-
-  //   infinite: true,
-  //   // speed: 500,
-  //   // fade: true,
-  // });
   $('.slider-list').slick({
     infinite: false,
+
     speed: 300,
     focusOnSelect: true,
     slidesToShow: 7,
     slidesToScroll: 7,
-    // centerMode: true,
     asNavFor: '.slider-container',
     responsive: [
-      // {
-      //   breakpoint: 1200,
-      //   settings: {
-      //     slidesToShow: 7,
-      //     slidesToScroll: 1,
-      //   },
-      // },
-
       {
         breakpoint: 767,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          // centerMode: true,
         },
       },
       // You can unslick at a given breakpoint now by adding:
